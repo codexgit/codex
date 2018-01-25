012544b0e8328e62dd62d6d9954b8a54783122da
Jacob Vega <jacobvega@MacBook-Air-de-Jacob.local>
2018-01-25 10:22:25 -0300

    Actualizacion archivos configuracion

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://localhost/codexgit';
+//$config['base_url'] = 'http://sibtra.puertoespconsultores.cl/';
 
 /*
 |--------------------------------------------------------------------------
